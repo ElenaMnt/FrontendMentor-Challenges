3fb08c005e89dfc112f99037efafebfa33960cec
ElenaMnt <84120962+ElenaMnt@users.noreply.github.com>
2021-09-02 17:19:17 +0300

    Needs update

--- SunnysideAgency/index.js
+++ SunnysideAgency/index.js
@@ -1,3 +1,4 @@
+// Scroll Down 
 let slide = document.querySelector('.arrow-down');
 
 slide.addEventListener("click", function(){
@@ -7,3 +8,15 @@ slide.addEventListener("click", function(){
      behavior: 'smooth'
     });
 });
+
+
+// Hamburger Menu Functionality
+const hamburger = document.querySelector('.nav-hamburger');
+const navUL = document.querySelector('.nav-ul');
+const title = document.querySelector('h1');
+
+
+hamburger.addEventListener('click', () => {
+    navUL.classList.toggle('show');
+    title.classList.toggle('remove');
+})
