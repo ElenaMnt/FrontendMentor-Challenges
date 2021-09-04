66d3f89f5359f18a2336459469fe0bd8c5cb1655
ElenaMnt <84120962+ElenaMnt@users.noreply.github.com>
2021-09-04 21:42:25 +0300

    Added a toggle functionality. To be updated

---
+++ PricingComponentToggle/PricingComponentToggle/index.js
@@ -0,0 +1,23 @@
+const toggleBtn = document.querySelector('.toggleBtn');
+let boxPrice = document.getElementsByClassName('boxPrice');
+const monthlyPrices = ["19.99", "24.99", "39.99"];
+const annualPrices = ["199.99", "249.99", "399.99"];
+
+toggleBtn.addEventListener("click", () => {
+    toggleBtn.classList.toggle('checked');
+
+    changePriceFunc();
+})
+
+function changePriceFunc(){
+    if(toggleBtn.classList.contains('checked')){
+        for(let idx = 0 ; idx < annualPrices.length; idx++){
+            boxPrice[idx].innerHTML = `<span class="priceDollar">&dollar;</span> ${annualPrices[idx]}`;
+        }
+    }
+    else{      
+          for(let idx = 0 ; idx < monthlyPrices.length; idx++){
+            boxPrice[idx].innerHTML = `<span class="priceDollar">&dollar;</span> ${monthlyPrices[idx]}`;
+        }
+    }
+}
